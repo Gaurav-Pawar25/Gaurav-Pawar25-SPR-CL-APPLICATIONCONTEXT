9c5aee515ae444b900b73ef4f1916dd65edd3c45
Gaurav-Pawar25 <gauravpawar33915@gmail.com>
2024-07-24 22:16:45 +0530

    Done

--- src/main/java/Lab/Lab.java
+++ src/main/java/Lab/Lab.java
@@ -5,6 +5,7 @@ import Lab.Beans.GasEngine;
 import Lab.Beans.NuclearEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,21 +22,24 @@ public class Lab {
      * ElectricEngine. There is no need to modify this method.
      * @return an ElectricEngine bean
      */
+    @Bean
     public ElectricEngine getElectricPoweredBean(){
-        return applicationContext.getBean(ElectricEngine.class);
+        return new ElectricEngine();
     }
     /**
      * TODO: Retrieve an GasEngine bean and return it.
      * @return a GasEngine bean
      */
+    @Bean
     public GasEngine getGasPoweredBean(){
-        return null;
+        return new GasEngine();
     }
     /**
      * TODO: Retrieve a NuclearEngine bean and return it.
      * @return a NuclearEngine bean
      */
+    @Bean
     public NuclearEngine getNuclearPoweredBean(){
-        return null;
+        return new NuclearEngine();
     }
 }
